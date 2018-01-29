143e8bb49f712a245139518b8f8a2bf541869138
Untitled <john.d.reddock@gmail.com>
2018-01-29 16:57:59 -0500

    Animations Part 1

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -5,18 +5,24 @@ import { Location } from '@angular/common';
 import { Comment } from '../shared/comment';
 import { Dish } from '../shared/dish';
 import { DishService } from '../services/dish.service';
-
-
-// import { MdSliderModule } from '@angular/material';
-// import { MdInputModule } from '@angular/material';
 import 'rxjs/add/operator/switchMap';
-
-
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
-  styleUrls: ['./dishdetail.component.scss']
+  styleUrls: ['./dishdetail.component.scss'],
+  animations: [
+    trigger('visibility', [
+      state('shown', style({
+        transform: 'scale(1.0)', opacity: 1
+      })),
+      state('hidden', style({
+        transform: 'scale(0.5)', opacity: 0
+    })),
+    transition('* => *', animate('0.5s ease-in-out'))
+    ])
+  ]
 })
 export class DishdetailComponent implements OnInit {
   dishcopy = null;
@@ -27,6 +33,7 @@ export class DishdetailComponent implements OnInit {
   dish : Dish;
   comment: Comment;
   errMess: string;
+  visibility = 'shown';
   
   formErrors = {
     'comment': '',
@@ -55,8 +62,10 @@ export class DishdetailComponent implements OnInit {
 
     this.dishservice.getDishIds().subscribe(dishIds => this.dishIds = dishIds);
     this.route.params 
-      .switchMap((params : Params) => {return this.dishservice.getDish(+params['id']); })
-      .subscribe(dish=> {this.dish = dish; this.dishcopy = dish; this.setPrevNext(dish.id); },
+      .switchMap((params : Params) => {this.visibility = 'hidden'; 
+        return this.dishservice.getDish(+params['id']); })
+      .subscribe(dish=> {this.dish = dish; this.dishcopy = dish; this.setPrevNext(dish.id); 
+        this.visibility = 'shown' },
         errmess => {this.dish = null; this.errMess = <any>errmess;});
     }
   createForm(){
